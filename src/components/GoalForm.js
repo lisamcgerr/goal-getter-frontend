06b9902b79865c55f0f0916ec1f6b1d9251317e1
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-03 12:15:44 -0500

    adding GoalForm.js to components

---
+++ src/components/GoalForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class GoalForm extends React.Component {
+  render(){
+    return(
+      <>
+      This is the GoalForm component
+      </>
+    )
+  }
+}
+
+export default GoalForm
