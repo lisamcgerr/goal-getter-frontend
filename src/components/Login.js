5b57c0c892ea4ff7ae115fea7d72c8ad2526269d
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-02 20:31:33 -0500

    signup and login components created

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,78 @@
 import React from 'react'
+import { Button, Segment, Form, Grid } from 'semantic-ui-react'
 
 
 class Login extends React.Component {
+  state = {
+    username: '',
+    password: '',
+    error: ''
+  }
+
+  handleInput = (e) => { 
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+}
+
+// handleSubmit = (e) => {
+//     e.preventDefault()
+
+//     const reqObj = {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body:  JSON.stringify(this.state)
+//     }
+
+//     fetch('http://localhost:3000/login', reqObj)
+//     .then(resp => resp.json())
+//     .then(data => {
+//         console.log('data', data)
+//         if (data.error){
+//             this.setState({
+//                 error: data.error
+//             })
+//         } else {
+//             this.props.loginSuccess(data.user)
+//             localStorage.setItem('myAppToken', data.token)
+//             this.setState({
+//                 username: '',
+//                 password: '',
+//                 error: ''})
+//             this.props.history.push('/home')
+//         }
+//     })
+// }
+  
   render(){
     return(
       <>
-      This is the Login component
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='top'>
+        <Grid.Column style={{ maxWidth: 550 }}>
+        <h1>Login</h1>
+          <Form onSubmit={this.handleSubmit} size='large'>
+            <Segment inverted stacked>
+              <Form.Input fluid icon='user' iconPosition='left' name={'username'} onChange={this.handleInput} value={this.state.username} placeholder='username' />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  type='password'
+                  iconPosition='left'
+                  name={'password'} 
+                  onChange={this.handleInput} 
+                  value={this.state.password} 
+                  placeholder='password'
+                />
+
+                <Button color='green' fluid size='large'>
+                  Login
+                </Button>
+            </Segment>
+          </Form>
+       </Grid.Column>
+      </Grid>
       </>
     )
   }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,69 @@
 import React from 'react'
+import { Button, Segment, Form, Grid } from 'semantic-ui-react'
 
 
 class Signup extends React.Component {
+
+  state = {
+    username: '',
+    password: ''
+  }
+
+  // handleChange = (e) => {
+  //   this.setState({
+  //       [e.target.name]: e.target.value
+  //   })
+  // }
+
+  // handleSubmit = (e) => {
+  //   e.preventDefault()
+
+  //   const newUser = {...this.state}
+  //   console.log(newUser)
+
+  //   const reqObj = {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Accept: 'application/json'
+  //       },
+  //       body:  JSON.stringify({user: newUser})
+  //   }
+  //   fetch('http://localhost:3000/users', reqObj)
+  //   .then(resp => resp.json())
+  //   .then(data => {
+  //     this.props.addUser(data)
+  //     this.props.history.push('/login')
+  //   })
+  // }
+
   render(){
     return(
       <>
-      This is the Signup component
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='top'>
+        <Grid.Column style={{ maxWidth: 550 }}>
+        <h1>Sign Up</h1>
+          <Form onSubmit={this.handleSubmit} size='large'>
+            <Segment inverted stacked>
+              <Form.Input fluid icon='user' iconPosition='left' name={'username'} onChange={this.handleInput} value={this.state.username} placeholder='username' />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  type='password'
+                  iconPosition='left'
+                  name={'password'} 
+                  onChange={this.handleInput} 
+                  value={this.state.password} 
+                  placeholder='password'
+                />
+
+                <Button color='green' fluid size='large'>
+                  Sign Up
+                </Button>
+            </Segment>
+          </Form>
+       </Grid.Column>
+      </Grid>
       </>
     )
   }
