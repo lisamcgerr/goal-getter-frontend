664d549743070ad0e159b4b220ff9f013152c5b7
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-03 15:01:20 -0500

    signup connected

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, Segment, Form, Grid } from 'semantic-ui-react'
-
+import { connect } from 'react-redux'
+import { createUser } from '../actions/index'
 
 class Signup extends React.Component {
 
@@ -9,33 +10,33 @@ class Signup extends React.Component {
     password: ''
   }
 
-  // handleChange = (e) => {
-  //   this.setState({
-  //       [e.target.name]: e.target.value
-  //   })
-  // }
+  handleChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+  }
 
-  // handleSubmit = (e) => {
-  //   e.preventDefault()
+  handleSubmit = (e) => {
+    e.preventDefault()
 
-  //   const newUser = {...this.state}
-  //   console.log(newUser)
+    const newUser = {...this.state}
+    console.log(newUser)
 
-  //   const reqObj = {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Accept: 'application/json'
-  //       },
-  //       body:  JSON.stringify({user: newUser})
-  //   }
-  //   fetch('http://localhost:3000/users', reqObj)
-  //   .then(resp => resp.json())
-  //   .then(data => {
-  //     this.props.addUser(data)
-  //     this.props.history.push('/login')
-  //   })
-  // }
+    const reqObj = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body:  JSON.stringify({user: newUser})
+    }
+    fetch('http://localhost:3000/users', reqObj)
+    .then(resp => resp.json())
+    .then(data => {
+      this.props.addUser(data)
+      this.props.history.push('/login')
+    })
+  }
 
   render(){
     return(
