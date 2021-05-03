dba4bcfdb260787f76057270810508d4835c9de9
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-03 06:16:57 -0500

    action types added

---
+++ src/actions/Actiontypes.js
@@ -0,0 +1,10 @@
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const CURRENT_USER = 'CURRENT_USER'
+
+export const CREATE_USER  = 'CREATE_USER'
+export const DELETE_USER  = 'DELETE_USER'
+export const UPDATE_USER  = 'UPDATE_USER'
+
+export const CREATE_GOAL  = 'CREATE_GOAL'
+export const DELETE_GOAL  = 'DELETE_GOAL'
+export const UPDATE_GOAL  = 'UPDATE_GOAL'
